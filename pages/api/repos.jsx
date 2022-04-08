072b03ada6bc785f96098b5f06b3bdf503d87291
TR/A <95937626+xrqujs@users.noreply.github.com>
2022-04-08 18:51:16 +0300

    Update repos.jsx

--- pages/api/repos.jsx
+++ pages/api/repos.jsx
@@ -1,18 +1,17 @@
 const fetch = require('isomorphic-unfetch') 
 
 let key1 = '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';
-let key2 = '91e65076097c64ec331f'; 
 
 export default async (req, res) => {
     let _ = await (await fetch('https://api.github.com/users/xrqujs/repos', {
         headers: {
-            Authorization: 'token '+key1+key2
+            Authorization: 'token '+key1
         }
     })).json(); 
 
     let __ = await (await fetch('https://api.github.com/users/xlqudev/repos', {
         headers: {
-            Authorization: 'token '+key1+key2
+            Authorization: 'token '+key1
         }
     })).json();
     
