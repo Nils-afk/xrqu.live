77fe50f90722e6432f8692c3423c9fd6fecd4372
TR/A <95937626+xrqujs@users.noreply.github.com>
2022-04-08 20:01:23 +0300

    Update repos.jsx

--- pages/api/repos.jsx
+++ pages/api/repos.jsx
@@ -1,7 +1,6 @@
 const fetch = require('isomorphic-unfetch') 
 
 let key1 = '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';
-let key2 = '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'
 
 export default async (req, res) => {
     let _ = await (await fetch('https://api.github.com/users/xrqujs/repos', {
