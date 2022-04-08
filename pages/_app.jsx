2c8c0a8c4dc8a24e6d3741329557933de666f69d
TR/A <95937626+xrqujs@users.noreply.github.com>
2022-04-08 18:18:22 +0300

    Update _app.jsx

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -37,7 +37,7 @@ function MyApp({ Component, pageProps }) {
     });
   return (<>
     <Head>
-        <title>clqu.live</title>
+        <title>xrqu.live</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="https://pro.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet" />
 
@@ -55,7 +55,7 @@ function MyApp({ Component, pageProps }) {
         <div style={{ zIndex: 99999 }} className="fixed bg-black/50 w-full h-screen flex justify-center items-center pointer-events-none">
             <div className="flex items-center gap-x-6 animate-pulse">
                 <div className="text-center">
-                    <p className="text-6xl mb-5 font-semibold">clqu.live</p>
+                    <p className="text-6xl mb-5 font-semibold">xrqu.live</p>
                     <p className="uppercase text-xl font-semibold text-white"><i className="fal fa-spinner-third fa-spin" /></p>
                 </div>
             </div>
@@ -70,11 +70,11 @@ function MyApp({ Component, pageProps }) {
         <div className="max-w-screen-lg p-5 w-full md:w-10/12 lg:w-8/12 mx-auto transition-all duration-300">
           <div className="md:flex w-full items-center justify-between">
             <div>
-              <p>Copyright &copy; 2022 - Developed with ❤️ by clqu.</p>
-              <a href="https://github.com/clqu/clqu.live" target="_blank" rel="noreferrer" className="hover:text-red-500 transition-all duration-200">This website is open source on GitHub.</a>
+              <p>Copyright &copy; 2022 - Developed with ❤️ by xrqu.</p>
+              <a href="https://github.com/xrqujs/xrqu.live" target="_blank" rel="noreferrer" className="hover:text-red-500 transition-all duration-200">This website is open source on GitHub.</a>
             </div>
             <div className="mt-2 md:mt-0 flex items-center">
-              <a href="https://voiddevs.org/discord" target="_blank" rel="noreferrer" className="w-full md:w-auto rounded-lg bg-[#191932]/20 p-2 px-6 hover:bg-[#191932]/30 shadow-lg shadow-white/0 hover:shadow-3xl hover:shadow-[#191932]/20 transition-all duration-200">
+              <a href="https://discord.com/users/850299286595698718" target="_blank" rel="noreferrer" className="w-full md:w-auto rounded-lg bg-[#191932]/20 p-2 px-6 hover:bg-[#191932]/30 shadow-lg shadow-white/0 hover:shadow-3xl hover:shadow-[#191932]/20 transition-all duration-200">
                 <i className="fab fa-discord mr-2" />Contact with Discord
               </a>
             </div>
