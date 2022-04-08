030d0376c1441055b1b2a94fe7e3f837e20ab7f3
TR/A <95937626+xrqujs@users.noreply.github.com>
2022-04-08 18:19:37 +0300

    Update contact.jsx

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -14,28 +14,28 @@ export default function Home() {
         <p className="text-xl text-white/50 font-normal text-center mb-5">To contact me, you can get information from the section below.</p>
         <div className=" flex justify-center flex-col items-center">
             <div className="max-w-lg w-full">
-                <a target="_blank" rel="norefferer" href="https://instagram.com/this.clqu">
+                <a target="_blank" rel="norefferer" href="https://instagram.com/xrqu.live">
                     <div className="flex justify-between bg-[#191932]/20 hover:bg-[#191932]/30 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-instagram fa-2x" />
-                        <p className="font-semibold text-xl">@this.clqu</p>
+                        <p className="font-semibold text-xl">@xrqu.live</p>
                     </div>
                 </a>
-                <a target="_blank" rel="norefferer" href="https://twitter.com/clquu_">
+                <a target="_blank" rel="norefferer" href="https://twitter.com/xrqulivee">
                     <div className="mt-2 flex justify-between bg-[#191932]/20 hover:bg-[#191932]/30 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-twitter fa-2x" />
-                        <p className="font-semibold text-xl">@clquu_</p>
+                        <p className="font-semibold text-xl">@xrqulivee</p>
                     </div>
                 </a>
-                <a target="_blank" rel="norefferer" href="https://discord.com/users/714451348212678658">
+                <a target="_blank" rel="norefferer" href="https://discord.com/users/850299286595698718">
                     <div className="mt-2 flex justify-between bg-[#191932]/20 hover:bg-[#191932]/30 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fab fa-discord fa-2x" />
-                        <p className="font-semibold text-xl">clqu#0604</p>
+                        <p className="font-semibold text-xl">xrqu#4567</p>
                     </div>
                 </a>
-                <a href="mailto:me@clqu.live">
+                <a href="mailto:me@xrqu-live.tk">
                     <div className="mt-2 flex justify-between bg-[#191932]/20 hover:bg-[#191932]/30 transition-all duration-200 px-4 py-2 rounded-lg items-center">
                         <i className="fa fa-envelope fa-2x" />
-                        <p className="font-semibold text-xl">me@clqu.live</p>
+                        <p className="font-semibold text-xl">me@xrqu-live.tk</p>
                     </div>
                 </a>
             </div>
