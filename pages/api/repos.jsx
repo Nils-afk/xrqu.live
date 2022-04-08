758927831d609fa1010646e051d847a1377a3682
TR/A <95937626+xrqujs@users.noreply.github.com>
2022-04-08 21:42:09 +0300

    Update repos.jsx

--- pages/api/repos.jsx
+++ pages/api/repos.jsx
@@ -5,9 +5,19 @@ let key1 = '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';
 export default async (req, res) => {
     let _ = await (await fetch('https://api.github.com/users/xrqujs/repos', {
         headers: {}
-    })).json(); 
+    })).json(); 
+  
+export default async (req, res) => {
+    let _ = await (await fetch('https://api.github.com/users/xlqudev/repos', {
+        headers: {}
+    })).json();
+
+export default async (req, res) => {
+    let _ = await (await fetch('https://api.github.com/users/vitobot-ml/repos', {
+        headers: {}
+    })).json();
 try {
-        res.send([..._])
+        res.send([..._,...__,...___])
     } catch {
         res.status(500);
     }
